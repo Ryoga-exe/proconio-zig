3bda9618993d74292e02e3b4b67f3690bb3e11d7
Ryoga-exe <git@ryoga.dev>
2025-02-05 18:49:41 +0900

    chore: update zig version

--- build.zig
+++ build.zig
@@ -19,7 +19,7 @@ pub fn build(b: *std.Build) void {
         .name = "proconio",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "proconio.zig" },
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -29,19 +29,23 @@ pub fn build(b: *std.Build) void {
     // running `zig build`).
     b.installArtifact(lib);
 
+    _ = b.addModule("proconio", .{
+        .root_source_file = b.path("src/root.zig"),
+    });
+
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
-    const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "proconio.zig" },
+    const lib_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    const run_main_tests = b.addRunArtifact(main_tests);
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build test`
-    // This will evaluate the `test` step rather than the default, which is "install".
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&run_main_tests.step);
+    // Similar to creating the run step earlier, this exposes a `test` step to
+    // the `zig build --help` menu, providing a way for the user to request
+    // running the unit tests.
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
 }
